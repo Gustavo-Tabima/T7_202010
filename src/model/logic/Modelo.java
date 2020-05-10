7855b1e6844feb2bdfa5e64ad721e8d399f02f5f
Gustavo-Tabima <ga.tabima@uniandes.edu.co>
2020-05-09 21:53:55 -0500

    Creados algoritmo de carga, falta instanciar

--- src/model/logic/Modelo.java
+++ src/model/logic/Modelo.java
@@ -1,7 +1,11 @@
 package model.logic;
 
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
@@ -17,7 +21,9 @@ import com.google.gson.JsonParser;
 import com.google.gson.stream.JsonReader;
 
 import model.data_structures.ElementoNoExisteException;
+import model.data_structures.GrafoNoDirigido;
 import model.data_structures.MaxPQ;
+import model.data_structures.Vertex;
 
 
 
@@ -26,8 +32,9 @@ import model.data_structures.MaxPQ;
  * @param <T>
  */
 public class Modelo<T extends Comparable<T> > {
-	public static String PATH = "./data/Comparendos_DEI_2018_Bogota_D.C.geojson";
-	//public static String PATH = "./data/Comparendos_DEI_2018_Bogota_D.C_small.geojson";
+	public static String PATHARCOS = "./data/bogota_arcos.txt";
+	public static String PATH = "./data/Comparendos_DEI_2018_Bogota_D.C_small.geojson";
+	public static String PATHVERTEX = "./data/bogota_vertices.txt";
 	public static String PATHPOLICE = "./data/estacionespoli.geojson";
 	public MallaVial mallavial;
 	public MaxPQ<Comparendo> comparendosCola = new MaxPQ<Comparendo>();
@@ -39,51 +46,57 @@ public class Modelo<T extends Comparable<T> > {
 
 	}
 
-	public void cargarDatosComparendos() {
-
-
-		JsonReader reader;
-
+	public void cargarDatosGrafo() {
+		File archivo = new File(PATHVERTEX);
+		File archivoA = new File(PATHARCOS);
+		FileReader fr;
 		try {
-			reader = new JsonReader(new FileReader(PATH));
-			JsonElement elem = JsonParser.parseReader(reader);
-			JsonArray e2 = elem.getAsJsonObject().get("features").getAsJsonArray();
+			fr = new FileReader(archivo);
+			BufferedReader bf = new BufferedReader(fr);
+			int lineas = 0;
+			while (bf.readLine() != null) { lineas++;
+			}
+			String linea = bf.readLine();
+			GrafoNoDirigido<Integer, Double, Double>  grafo = new GrafoNoDirigido<Integer, Double, Double>(lineas);
+			while(linea !=null){
+				String [] lineasnuevas =  linea.split(",");
 
-			SimpleDateFormat parser = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSXXX");
-
-			for (int i = 0; i < e2.size();i++) {
-				JsonElement e = e2.get(i);
-				int OBJECTID = e.getAsJsonObject().get("properties").getAsJsonObject().get("OBJECTID").getAsInt();
-
-				String s = e.getAsJsonObject().get("properties").getAsJsonObject().get("FECHA_HORA").getAsString();
-				Date FECHA_HORA = parser.parse(s);
-
-				String MEDIO_DETE = e.getAsJsonObject().get("properties").getAsJsonObject().get("MEDIO_DETECCION").getAsString();
-				String CLASE_VEHI = e.getAsJsonObject().get("properties").getAsJsonObject().get("CLASE_VEHICULO").getAsString();
-				String TIPO_SERVI = e.getAsJsonObject().get("properties").getAsJsonObject().get("TIPO_SERVICIO").getAsString();
-				String INFRACCION = e.getAsJsonObject().get("properties").getAsJsonObject().get("INFRACCION").getAsString();
-				String DES_INFRAC = e.getAsJsonObject().get("properties").getAsJsonObject().get("DES_INFRACCION").getAsString();
-				String LOCALIDAD = e.getAsJsonObject().get("properties").getAsJsonObject().get("LOCALIDAD").getAsString();
-				String MUNICIPIO = e.getAsJsonObject().get("properties").getAsJsonObject().get("MUNICIPIO").getAsString();
-
-				double longitud = e.getAsJsonObject().get("geometry").getAsJsonObject().get("coordinates").getAsJsonArray()
-						.get(0).getAsDouble();
-
-				double latitud = e.getAsJsonObject().get("geometry").getAsJsonObject().get("coordinates").getAsJsonArray()
-						.get(1).getAsDouble();
-
-
-				Comparendo c = new Comparendo(OBJECTID, FECHA_HORA, MEDIO_DETE, CLASE_VEHI, TIPO_SERVI, INFRACCION, DES_INFRAC, LOCALIDAD, MUNICIPIO, longitud, latitud);
-				comparendosCola.insert(c);
-
-
-			}}
-		catch (Exception e){
-			System.out.println(e.getMessage());
+//				Vertex<Integer,Double,Double> n = new Vertex<Integer, Double, Double>(Integer.parseInt(lineasnuevas[0]),Double.parseDouble(lineasnuevas[1]));
+				linea = bf.readLine();
+				
+			}
+			bf.close();
+			fr.close();
+			
+			
+		} catch (  IOException e ) {
+			// TODO Auto-generated catch block
 			e.printStackTrace();
+			
 		}
-	}
+		try {
+		FileReader fra = new FileReader(archivoA);
+		BufferedReader bfa = new BufferedReader(fra);
+		
+		String lineaA = bfa.readLine();
+		while(lineaA !=null){
+			String [] lineasnuevasA =  lineaA.split(" ");
 
+//			Aqui debería ir la creación de cada Arco según los vértices creados anteriormente.
+			lineaA = bfa.readLine();
+			
+		}
+		
+		bfa.close();
+		fra.close();
+		
+		
+		} catch (IOException ex) {
+			ex.printStackTrace();
+		}
+		
+		
+	}
 
 	public void cargarDatosPolicia() {
 
